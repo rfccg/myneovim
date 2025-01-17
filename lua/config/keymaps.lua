3804649e14757ad93944a873587a515881f1b55e
rfccg <rafael.gouveia@incognia.com>
2025-01-17 15:12:35 -0300

    add keymap to navigate brackets

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local keymap = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+keymap("n", "m.", "//{<cr>", opts)
+keymap("n", "m,", "?{<cr>", opts)
+keymap("v", "p", '"_dP', opts)
+--keymap("v", "m", ":m .+1<CR>==", {})
+--keymap("v", "M", ":m .-2<CR>==", {})
---
+++ lua/plugins/auto-pairs.lua
@@ -0,0 +1,10 @@
+-- return {
+--   "windwp/nvim-autopairs",
+--   event = "InsertEnter",
+--   config = true,
+--   opts = {
+--     disable_in_macro = true,
+--   },
+--   -- use opts = {} for passing setup options
+--   -- this is equivalent to setup({}) function
+-- }
